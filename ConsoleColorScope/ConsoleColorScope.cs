407fc32d2a9e42d680df5465bb689a26ed32a732
fo40225 <fo40225@hotmail.com>
2016-02-27 20:39:13 +0800

    implement ConsoleColorScope

---
+++ ConsoleColorScope/ConsoleColorScope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleColorScope
+{
+    public class ConsoleColorScope : IDisposable
+    {
+        private readonly ConsoleColor previousColor;
+
+        public ConsoleColorScope(ConsoleColor color)
+        {
+            this.previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+        }
+
+        public void Dispose()
+        {
+            Console.ForegroundColor = this.previousColor;
+        }
+    }
+}
